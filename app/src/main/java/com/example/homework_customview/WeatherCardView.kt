82124933a823e79641ecf37c39a613bca76980e1
Farkhat <farhad020202@gmail.com>
2023-11-01 19:05:31 +0600

    2

--- app/src/main/java/com/example/homework_customview/WeatherCardView.kt
+++ app/src/main/java/com/example/homework_customview/WeatherCardView.kt
@@ -2,12 +2,10 @@ package com.example.homework_customview
 
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import androidx.core.content.ContextCompat
 import com.example.homework_customview.databinding.WeatherCardBinding
 
 class WeatherCardView @JvmOverloads constructor(
@@ -17,49 +15,84 @@ class WeatherCardView @JvmOverloads constructor(
     private val binding: WeatherCardBinding =
         WeatherCardBinding.inflate(LayoutInflater.from(context), this, true)
 
-    init {
-        setAttrs(attrs, R.styleable.WeatherCardView) {
-            binding.tvLocation.text = it.getString(R.styleable.WeatherCardView_city_name)
-            binding.tvTemperature.text = it.getString(R.styleable.WeatherCardView_temperature)
-            binding.tvWeatherCondition.text =
-                it.getString(R.styleable.WeatherCardView_weather_description)
-
-            binding.ivWeatherIcon.setImageResource(
-                it.getResourceId(
-                    R.styleable.WeatherCardView_weather_icon,
-                    R.drawable.ic_moon_cloud_mid_rain
-                )
-            )
-
-
-        }
-    }
-
-    var cityName: String
-        get() = binding.tvLocation.text.toString()
+    var cityName: String = ""
         set(value) {
-            binding.tvLocation.text = value
+            field = value
+            updateLocation()
         }
 
-    var temperature: String
-        get() = binding.tvTemperature.text.toString()
+    var country: String = ""
         set(value) {
+            field = value
+            updateLocation()
+        }
+
+    var temperature: String = ""
+        set(value) {
+            field = value
             binding.tvTemperature.text = value
         }
 
-    var weatherDescription: String
-        get() = binding.tvWeatherCondition.text.toString()
+    var timeOfDay: Int = 0
         set(value) {
-            binding.tvWeatherCondition.text = value
+            field = value
+            updateWeatherIcon()
         }
 
-    var weatherIcon: Drawable?
-        get() = binding.ivWeatherIcon.drawable
+    var precipitationType: Int = 0
         set(value) {
-            binding.ivWeatherIcon.setImageDrawable(value)
+            field = value
+            updateWeatherConditions()
+            updateWeatherIcon()
         }
+
+    var hasWind: Boolean = false
+        set(value) {
+            field = value
+            updateWeatherConditions()
+            updateWeatherIcon()
+        }
+
+    init {
+        setAttrs(attrs, R.styleable.WeatherCardView) {
+            cityName = it.getString(R.styleable.WeatherCardView_city_name) ?: ""
+            country = it.getString(R.styleable.WeatherCardView_country) ?: ""
+            temperature = it.getString(R.styleable.WeatherCardView_temperature) ?: ""
+            timeOfDay = it.getInt(R.styleable.WeatherCardView_timeOfDay, 0)
+            precipitationType = it.getInt(R.styleable.WeatherCardView_precipitationType, 0)
+            hasWind = it.getBoolean(R.styleable.WeatherCardView_hasWind, false)
+        }
+    }
+
+    private fun updateLocation() {
+        binding.tvLocation.text = "$cityName, $country"
+    }
+
+    private fun updateWeatherConditions() {
+        val weatherConditions = mutableListOf<String>().apply {
+            when (precipitationType) {
+                0 -> add(context.getString(R.string.light_rain))
+                1 -> add(context.getString(R.string.heavy_rain))
+                2 -> add(context.getString(R.string.light_snow))
+                3 -> add(context.getString(R.string.tornado))
+            }
+            if (hasWind) add(context.getString(R.string.wind))
+        }
+        binding.tvWeatherCondition.text = weatherConditions.joinToString(", ")
+    }
+
+    private fun updateWeatherIcon() {
+        binding.ivWeatherIcon.setImageResource(
+            when {
+                precipitationType == 3 -> R.drawable.ic_tornado
+                timeOfDay == 1 && hasWind -> R.drawable.ic_moon_cloud_fast_wind
+                timeOfDay == 1 && precipitationType == 0 -> R.drawable.ic_moon_cloud_mid_rain
+                timeOfDay == 0 && precipitationType == 1 -> R.drawable.ic_sun_cloud_angled_rain
+                else -> R.drawable.ic_sun_cloud_angled_rain
+            }
+        )
+    }
 }
-
 
 inline fun View.setAttrs(
     attrs: AttributeSet?,
@@ -75,4 +108,3 @@ inline fun View.setAttrs(
             }
         }
 }
-
